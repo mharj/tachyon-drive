1a4fc3a1d2dd72933c249ae28a36d4f1b66536f1
Marko Harjula <marko.harjula@avanio.com>
2024-10-02 19:21:52 +0300

    update actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
 	test: {
-    reporters: process.env.GITHUB_ACTIONS ? ['github-actions'] : ['verbose'],
+		reporters: process.env.GITHUB_ACTIONS ? ['github-actions'] : ['verbose', 'github-actions', 'junit'],
+    outputFile: {
+      junit: './test-results.xml',
+    },
 		coverage: {
 			provider: 'v8',
-      include: ['src/**/*.ts'],
-      reporter: ['text'],
+			include: ['src/**/*.ts'],
+			reporter: ['text'],
 		},
 		include: ['test/**/*.test.ts'],
 	},
