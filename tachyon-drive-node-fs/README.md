c22bbfd4d941969f28329dfccd88a46a52c7ff86
Marko Harjula <marko.harjula@avanio.com>
2025-05-30 09:44:35 +0300

    tachyon-drive-node-fs import

---
+++ tachyon-drive-node-fs/README.md
@@ -0,0 +1,42 @@
+# tachyon-drive-node-fs
+
+[![TypeScript](https://badges.frapsoft.com/typescript/code/typescript.svg?v=101)](https://github.com/ellerbrock/typescript-badges/)
+[![npm version](https://badge.fury.io/js/tachyon-drive-node-fs.svg)](https://badge.fury.io/js/tachyon-drive-node-fs)
+[![Maintainability](https://api.codeclimate.com/v1/badges/9ac69c287aa3f5ce94a4/maintainability)](https://codeclimate.com/github/mharj/tachyon-drive-node-fs/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/9ac69c287aa3f5ce94a4/test_coverage)](https://codeclimate.com/github/mharj/tachyon-drive-node-fs/test_coverage)
+![github action](https://github.com/mharj/tachyon-drive-node-fs/actions/workflows/main.yml/badge.svg?branch=main)
+
+## NodeFS File Storage Driver for Tachyon Drive and Tachyon Drive Crypto processor
+
+### Initialize simple JSON file storage driver
+
+```typescript
+const driver = new FileStorageDriver('FileStorageDriver', './store.json', bufferSerializer);
+```
+
+### Initialize crypt processor with JSON file storage driver
+
+```typescript
+const processor = new CryptoBufferProcessor(Buffer.from('some-secret-key'));
+const driver = new FileStorageDriver('FileStorageDriver', './store.json.aes', bufferSerializer, processor);
+```
+
+### Example usage of local notification file updates when running multiple process on same host
+
+```typescript
+const fileUpdateNotify = new FileUpdateNotify('./store.notify');
+const driver = new SomeDriver('SomeDriverWithoutUpdateNotification', bufferSerializer, fileUpdateNotify);
+```
+
+```typescript
+//  includes common strToBufferSerializer
+const jsonSerialization: IPersistSerializer<Data, string> = {
+	deserialize: (buffer: string) => JSON.parse(buffer.toString()),
+	serialize: (data: Data) => JSON.stringify(data),
+	validator: (data: Data) => dataSchema.safeParse(data).success,
+};
+
+const bufferSerializer: IPersistSerializer<Data, Buffer> = nextSerializer<Data, string, Buffer>(jsonSerialization, strToBufferSerializer);
+```
+
+### see more on NPMJS [tachyon-drive](https://www.npmjs.com/package/tachyon-drive)
---
+++ tachyon-drive-node-fs/src/drivers/FileStorageDriver.ts
@@ -0,0 +1,161 @@
+import {readFile, unlink, writeFile} from 'fs/promises';
+import {existsSync, type FSWatcher, watch} from 'node:fs';
+import type {ILoggerLike} from '@avanio/logger-like';
+import {type Loadable, toError} from '@luolapeikko/ts-common';
+import {type IPersistSerializer, type IStoreProcessor, StorageDriver, TachyonBandwidth} from 'tachyon-drive';
+
+export type FileStorageDriverOptions = {
+	/**
+	 * File name or async function that returns a file name
+	 */
+	fileName: Loadable<string>;
+	/**
+	 * Speed of the file storage driver, default is "TachyonBandwidth.Large".
+	 */
+	bandwidth?: TachyonBandwidth;
+};
+
+/**
+ * A storage driver that uses the local file system to store files.
+ */
+export class FileStorageDriver<Input> extends StorageDriver<Input, Buffer> {
+	public readonly bandwidth: TachyonBandwidth;
+	private isWriting = false;
+	private fileName: Loadable<string>;
+	private fileWatch: FSWatcher | undefined;
+
+	private fileChangeTimeout: ReturnType<typeof setTimeout> | undefined;
+
+	/**
+	 * Creates a new instance of the `FileStorageDriver` class.
+	 * @param name - name of the driver
+	 * @param options - options for the driver
+	 * @param options.fileName - file name or async function that returns a file name
+	 * @param options.bandwidth - speed of the file storage driver, default is "TachyonBandwidth.Large"
+	 * @param serializer - serializer to serialize and deserialize data (to and from Buffer)
+	 * @param processor - optional processor to process data before storing and after hydrating
+	 * @param logger - optional logger to log messages
+	 */
+	constructor(
+		name: string,
+		options: FileStorageDriverOptions,
+		serializer: IPersistSerializer<Input, Buffer>,
+		processor?: Loadable<IStoreProcessor<Buffer>>,
+		logger?: ILoggerLike,
+	) {
+		super(name, serializer, null, processor, logger);
+		this.bandwidth = options.bandwidth ?? TachyonBandwidth.Large;
+		this.fileName = options.fileName;
+		this.fileWatcher = this.fileWatcher.bind(this);
+	}
+
+	/**
+	 * initialize watcher if have a file
+	 */
+	protected async handleInit(): Promise<boolean> {
+		await this.setFileWatcher();
+		return true;
+	}
+
+	/**
+	 * unload watcher if have a file
+	 */
+	protected async handleUnload(): Promise<boolean> {
+		return this.unsetFileWatcher();
+	}
+
+	/**
+	 * Actual implementation of store data to the file
+	 */
+	protected async handleStore(buffer: Buffer): Promise<void> {
+		// buffer sanity check
+		if (!Buffer.isBuffer(buffer)) {
+			throw new TypeError(`FileStorageDriver '${this.name}' can only store Buffers`);
+		}
+		this.isWriting = true;
+		await writeFile(await this.getFileName(), buffer);
+		await this.setFileWatcher();
+		this.isWriting = false;
+	}
+
+	/**
+	 * Actual implementation of hydrate data from the file
+	 */
+	protected async handleHydrate(): Promise<Buffer | undefined> {
+		const fileName = await this.getFileName();
+		if (existsSync(fileName)) {
+			const buffer = await readFile(fileName);
+			await this.setFileWatcher();
+			return buffer;
+		}
+		return undefined;
+	}
+
+	/**
+	 * Actual implementation of delete file and unwatch it
+	 */
+	protected async handleClear(): Promise<void> {
+		await this.unsetFileWatcher();
+		const fileName = await this.getFileName();
+		if (existsSync(fileName)) {
+			this.isWriting = true;
+			await unlink(fileName);
+			this.isWriting = false;
+		}
+	}
+
+	/**
+	 * Set file watcher if file exists
+	 */
+	private async setFileWatcher() {
+		const fileName = await this.getFileName();
+		if (!this.fileWatch && existsSync(fileName)) {
+			this.fileWatch = watch(fileName, this.fileWatcher);
+		}
+	}
+
+	private unsetFileWatcher(): Promise<boolean> {
+		if (this.fileWatch) {
+			this.fileWatch.close();
+			return Promise.resolve(true);
+		}
+		return Promise.resolve(false);
+	}
+
+	/**
+	 * method for file watcher instance
+	 */
+	private fileWatcher(event: 'rename' | 'change') {
+		// ignore watcher events if writing
+		if (!this.isWriting && event === 'change') {
+			if (this.fileChangeTimeout) {
+				clearTimeout(this.fileChangeTimeout);
+			}
+			// delay to avoid multiple file change events
+			this.fileChangeTimeout = setTimeout(async () => {
+				this.fileChangeTimeout = undefined;
+				try {
+					await this.handleUpdate();
+				} catch (error) {
+					/* c8 ignore next 2 */
+					this.logger.error(`FileStorageDriver '${this.name}' failed to update data: ${toError(error).message}`);
+				}
+			}, 100);
+		}
+	}
+
+	/**
+	 * Build file name from fileNameOrPromise
+	 */
+	private async getFileName(): Promise<string> {
+		// lock down file name as this can't be changed after change events
+		if (typeof this.fileName === 'function') {
+			this.fileName = this.fileName();
+		}
+		const value = await this.fileName;
+		if (typeof value !== 'string') {
+			throw new TypeError(`FileStorageDriver '${this.name}' fileName argument must return a string, value: ${JSON.stringify(value)}`);
+		}
+		return value;
+	}
+}
